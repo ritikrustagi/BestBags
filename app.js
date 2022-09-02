e4137ef92c6efc8067c632bfbbd8a1ad625b6d49
Ritik Rustagi <rowd.ritrust@gmail.com>
2022-09-02 22:12:32 +0530

    first commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/admin", adminRouter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
