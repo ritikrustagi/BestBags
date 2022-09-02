8f97d8d420397ba28f37c420815555e44a67adcd
Ritik Rustagi <rowd.ritrust@gmail.com>
2022-09-02 22:53:09 +0530

    first commit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: 'abcde',//process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({
