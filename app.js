19c469a0d80b1c4533f9ad6dbde50ab3130aa608
Ritik Rustagi <rowd.ritrust@gmail.com>
2022-09-02 23:04:26 +0530

    first commit

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.set("port", port);
 app.listen(port, () => {
   console.log("Server running at port " + port);
